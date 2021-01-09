661b6b633e0de15de27e8ab9f69c32a6c2e87506
outisa <savolainen.outi@gmail.com>
2021-01-09 15:58:34 +0200

    Modify app.js

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ app.use(express.static('dist'))
 app.get('/health', (req, res) => {
   res.send('ok')
 })
-app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
-})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line
 })
